edc703fcf9e7909673cce0fdaf3cd5a943dd2d2a
Rafael-RT <toizhanov77@gmail.com>
2022-05-13 16:41:00 +0600

    first commit

---
+++ app/src/main/java/com/example/bookcave/HomeCustomer.java
@@ -0,0 +1,45 @@
+package com.example.bookcave;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.bookcave.extras.LogoutDialog;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class HomeCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_customer);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actionbarmenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.logoutButtonHeader:
+                openDialog();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    public void openDialog(){
+        LogoutDialog logoutdialog=new LogoutDialog();
+        logoutdialog.show(getSupportFragmentManager(),"Log out Dialog");
+    }
+}
